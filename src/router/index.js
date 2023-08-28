2b0c8278c0d2e9f135249459321291b126e3c16f
Adam <adam@bubalubs.io>
2023-08-28 04:53:57 +0100

    Remove unneeded stuff

--- src/router/index.js
+++ src/router/index.js
@@ -10,12 +10,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/stripe-3dsecure-2-checkout-using-setup-intent',
+      name: 'stripe-3dsecure-2-checkout-using-setup-intent',
+      component: () => import('../views/stripe-3dsecure-2-checkout-using-setup-intent.vue')
     }
   ]
 })
